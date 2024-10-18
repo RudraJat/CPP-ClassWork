8a05a733031ed7093af4a6b2c1cde82003e9bfe5
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-10-18 10:30:35 +0530

    Another way by making string

--- firstProg.cpp
+++ firstProg.cpp
@@ -1,20 +1,45 @@
+//It returns 2 integer
+// #include<iostream>
+// #include<utility>
+// using namespace std;
+// class Student{
+//     int rollno;
+//     int fees;
+//     public:
+//     Student(int a,int m){
+//         rollno=a;
+//         fees=m;
+//     }
+//     operator pair<int,int>(){
+//         return make_pair(rollno,fees);
+//     }
+// };
+// int main(){
+//     Student st(5,45000);
+//     pair<int,int> details=st;
+//     cout<<"Roll no: "<<details.first<<" Fees: "<<details.second;
+// }
+
+
+//2nd way
 #include<iostream>
-#include<utility>
+#include<string>
 using namespace std;
-class Student{
-    int rollno;
+class Stu{
+    int roll;
     int fees;
     public:
-    Student(int a,int m){
-        rollno=a;
+    Stu(int a, int m){
+        roll=a;
         fees=m;
     }
-    operator pair<int,int>(){
-        return make_pair(rollno,fees);
+    operator string(){
+        return "Roll No: "+to_string(roll)+", Fees: "+to_string(fees);
     }
 };
 int main(){
-    Student st(5,45000);
-    pair<int,int> details=st;
-    cout<<"Roll no: "<<details.first<<" Fees: "<<details.second;
+    Stu st(4, 45550);
+    string result =st;
+    cout<<"Details: "<<result<<endl;
+    return 0;
 }
