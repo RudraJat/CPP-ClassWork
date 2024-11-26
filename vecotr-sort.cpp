881f01e5021069dd32ae4bea4793be292dd792f2
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-11-26 11:28:50 +0530

    up

--- vecotr-sort.cpp
+++ vecotr-sort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     vector<int> vec ={1,5,8,9,6,7,3,2,0};
     sort(vec.begin(),vec.end());
-    for(int i=0;i<9;i++){
+    for(int i=0;i<vec.size();i++){
         cout<<vec[i]<<" ";
     }
     cout<<endl;
